36d5492887bfb040c043e988a6ad7955690af975
Siddharth Sharma <70106499+pao0318@users.noreply.github.com>
2021-01-29 21:05:16 +0530

    added webscrap code
    General code to obtain data of book title , upvotes and cover image of current best rated books grouped by their genre

---
+++ webscap.py
@@ -0,0 +1,31 @@
+import bs4
+from urllib.request import urlopen as uReq
+from bs4 import BeautifulSoup as soup
+
+my_url="https://www.goodreads.com/choiceawards/best-nonfiction-books-2020"
+uClient=uReq(my_url)
+page_html=uClient.read()
+uClient.close()
+
+page_soup=soup(page_html,"html.parser")
+
+containers=page_soup.findAll("div",{"class":"inlineblock pollAnswer resultShown"})
+
+filename="non-fiction_genre.csv"
+f=open(filename,'w')
+headers="Votes, Title, Cover_Image\n"
+
+f.write(headers)
+
+for container in containers:
+ 	votes=container.strong.text.split()[0]
+ 	title=container.div.div.a.img['alt']
+ 	cover_img=container.div.div.a.img['src']
+
+ 	print("Votes "+votes)
+ 	print("Title "+ title)
+ 	print("Cover_Image "+cover_img)
+ 	f.write(votes.replace(',','')+","+title+","+cover_img+"\n")
+	
+
+f.close()	
